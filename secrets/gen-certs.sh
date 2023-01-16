f8d8326ac51dac728d5f34cb3e50cb402c07c762
Ryan Kenney <rkenney@moesol.com>
2023-01-16 09:41:55 -0800

    * Improved cert generation * Fixed HAProxy certs config

--- secrets/gen-certs.sh
+++ secrets/gen-certs.sh
@@ -21,62 +21,85 @@ function generate_password() {
 }
 
 function create_root_cert() {
-    if [[ -f "${TARGET_DIR}/ca/root-ca.crt" ]]; then
+    local target_dir_ca="$1"
+    shift
+
+    if [[ -f "${target_dir_ca}/root-ca.crt" ]]; then
         echo "[Root CA Cert Already Exists]"
     else
         echo "[Creating Root CA Cert]"
-        openssl req -x509 -new -nodes -keyout "${TARGET_DIR}/ca/root-ca.key" -days 1024 -out "${TARGET_DIR}/ca/root-ca.crt" \
-            -passin "pass:$ca_pass" \
-            -subj "/C=US/ST=California/L=San Diego/O=DevOps/CN=Auth-Demo-RootCA"
+        openssl req -x509 -new -nodes -keyout "${target_dir_ca}/root-ca.crt.key" -days 1024 -out "${target_dir_ca}/root-ca.crt" \
+            -subj "/C=US/ST=California/L=San Diego/O=Auth-Demo/OU=Auth-Demo/CN=Auth-Demo-RootCA"
     fi
 }
 
 function create_machine_cert() {
+    local target_dir_ca="$1"
+    shift
+    local target_dir_machine="$1"
+    shift
     local machine_fqdn="$1"
     shift
 
-    if [[ -f "${TARGET_DIR}/${machine_fqdn}/cacerts.pem" ]]; then
-        echo "[Machine cacerts Already Exists]: ${machine_fqdn}"
-    else
-        echo "[Creating cacerts]: ${machine_fqdn}"
-        cp "${TARGET_DIR}/ca/root-ca.crt" "${TARGET_DIR}/${machine_fqdn}/cacerts.pem"
-    fi
-
-    if [[ -f "${TARGET_DIR}/${machine_fqdn}/${machine_fqdn}.csr" ]]; then
-        echo "[Machine CSR Already Exists]: ${machine_fqdn}"
+    if [[ -f "${target_dir_machine}/${machine_fqdn}.crt.key" ]]; then
+        echo "[Machine CSR and Key Already Exists]: ${machine_fqdn}"
     else
         echo "[Creating a Machine CSR and Key]: ${machine_fqdn}"
-        # NOTE: "-nodes" prevents the need for a challenge password since we're doing this all locally
-        openssl req -new -keyout "${TARGET_DIR}/${machine_fqdn}/${machine_fqdn}.key" \
-            -subj "/C=US/ST=California/L=San Diego/O=DevOps/CN=${machine_fqdn}" \
-            -nodes  \
-            -out "${TARGET_DIR}/${machine_fqdn}/${machine_fqdn}.csr"
+        # NOTE: Using a config file in order to specify SubjectAltName (now required by browsers)
+        cat << EOF > "${target_dir_machine}/${machine_fqdn}.req.config"
+[req]
+default_bits = 2048
+distinguished_name = req_distinguished_name
+req_extensions = req_ext
+prompt = no
+
+[req_distinguished_name]
+C = US
+ST = California
+L = San Diego
+O = Auth-Demo
+OU = Auth-Demo
+CN = ${machine_fqdn}
+
+[req_ext]
+subjectAltName = @alt_names
+
+[alt_names]
+DNS.1 = ${machine_fqdn}
+#DNS.2 = other.hostname
+#IP.1 = 192.168.1.1
+#IP.2 = 192.168.1.2
+EOF
+        openssl req -new -nodes \
+            -keyout "${target_dir_machine}/${machine_fqdn}.crt.key" \
+            -out "${target_dir_machine}/${machine_fqdn}.csr" \
+            -config "${target_dir_machine}/${machine_fqdn}.req.config"
     fi
 
-    if [[ -f "${TARGET_DIR}/${machine_fqdn}/${machine_fqdn}.crt" ]]; then
+    if [[ -f "${target_dir_machine}/${machine_fqdn}.crt" ]]; then
         echo "[Machine Cert Already Exists]: ${machine_fqdn}"
     else
         echo "[Creating a Machine Cert]: ${machine_fqdn}"
-        openssl x509 -req -in "${TARGET_DIR}/${machine_fqdn}/${machine_fqdn}.csr" -CA "${TARGET_DIR}/ca/root-ca.crt" \
-            -CAkey "${TARGET_DIR}/ca/root-ca.key" -passin "pass:$ca_pass" \
-            -CAcreateserial -out "${TARGET_DIR}/${machine_fqdn}/${machine_fqdn}.crt" -days 1024
+        openssl x509 -req -days 1024 \
+            `# Trust and include all extensions in the CSR` \
+            -copy_extensions copyall \
+            -in "${target_dir_machine}/${machine_fqdn}.csr" \
+            -CA "${target_dir_ca}/root-ca.crt" \
+            -CAkey "${target_dir_ca}/root-ca.crt.key" \
+            -CAcreateserial \
+            -out "${target_dir_machine}/${machine_fqdn}.crt" 
     fi
 
-    if [[ -f "${TARGET_DIR}/${machine_fqdn}/${machine_fqdn}.crt-key.pem" ]]; then
-        echo "[Machine Cert&Key Already Exists]: ${machine_fqdn}"
-    else
-        echo "[Creating a Machine Cert&Key]: ${machine_fqdn}"
-        cat "${TARGET_DIR}/${machine_fqdn}/${machine_fqdn}.crt" "${TARGET_DIR}/${machine_fqdn}/${machine_fqdn}.key" \
-            > "${TARGET_DIR}/${machine_fqdn}/${machine_fqdn}.crt-key.pem"
-    fi
+    # Create folder for HAProxy-specific files
+    mkdir -p "${target_dir_machine}/haproxy"
 
-    if [[ -f "${TARGET_DIR}/${machine_fqdn}/${machine_fqdn}.p7b" ]]; then
-        echo "[Machine pkcs7 File Already Exists]: ${machine_fqdn}"
+    # Generate cert+key file in the format expected by HAProxy
+    if [[ -f "${target_dir_machine}/haproxy/${machine_fqdn}.pem" ]]; then
+        echo "[Machine HAProxy Cert Already Exists]: ${machine_fqdn}"
     else
-        echo "[Creating a Machine pkcs7 File]: ${machine_fqdn}"
-        # NOTE: Java seems picky about the order of these certs, whereas Windows doesn't.
-        openssl crl2pkcs7 -nocrl -certfile "${TARGET_DIR}/${machine_fqdn}/${machine_fqdn}.crt" \
-            -certfile "${TARGET_DIR}/ca/root-ca.crt" -out "${TARGET_DIR}/${machine_fqdn}/${machine_fqdn}.p7b"
+        echo "[Creating a Machine HAProxy Cert]: ${machine_fqdn}"
+        cat "${target_dir_machine}/${machine_fqdn}.crt" "${target_dir_ca}/root-ca.crt" "${target_dir_machine}/${machine_fqdn}.crt.key" \
+            > "${target_dir_machine}/haproxy/${machine_fqdn}.pem"
     fi
 }
 
@@ -85,7 +108,14 @@ if [[ $# != 1 ]]; then
 fi
 TARGET_DIR="$1"
 
-create_root_cert
-create_machine_cert "*.auth-demo.docker"
+# Create certs
+create_root_cert "${TARGET_DIR}/ca-private"
+create_machine_cert "${TARGET_DIR}/ca-private" "${TARGET_DIR}/proxy" "bitbucket.proxy.auth-demo.docker"
+
+# Share the CA cert with all machines
+cp "${TARGET_DIR}/ca-private/root-ca.crt" "${TARGET_DIR}/ca-shared/root-ca.crt"
+
+# Change ownership to match the UID:GID of containers (making key files readable)
+find "${TARGET_DIR}/proxy" -type f -exec chown '99:99' {} \;
 
 echo "==== SUCCESS ===="
